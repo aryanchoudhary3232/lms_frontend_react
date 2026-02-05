ae1c0e4c1aaf6e740f3c81f3a2a426a50b3b38ed
Shreyaan <shreyaan.l23@iiits.in>
2026-02-05 20:56:49 +0530

    middlewares fix

---
+++ src/helper/middleware.js
@@ -0,0 +1,10 @@
+// Centralized middleware exports for React frontend
+import ProtectedRoute from "./ProtectedRoute";
+
+// Re-export all middleware components
+export { ProtectedRoute };
+
+// Default export for convenience
+export default {
+  ProtectedRoute,
+};
